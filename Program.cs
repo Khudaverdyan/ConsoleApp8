5b0175b2f615b302c2442bf4c43863f04c3f0e76
User <User@ADMIN>
2023-06-04 18:36:16 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            for (int j = 50; j <70; j++)
+                 
+            {
+                int result = 1;
+                for(int i = 2; i <=j;i++)
+                {
+                    while (i % 2 == 0)
+                    {
+                        result = i / 2;
+                        Console.WriteLine(result);
+
+
+                       if (i % 2 == 1)
+                        {
+                            result = ((i * 3) + 1) / 2;
+                            Console.WriteLine(result);
+                        }
+                    }
+                        break;
+                   
+                }
+               
+
+
+            }
+        }
+    }
+}
